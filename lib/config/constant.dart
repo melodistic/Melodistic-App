9c4d7e43702213b974d42cbb272f23bba82e975e
coolkidneversleep <iiippbhy@gmail.com>
2022-04-23 11:36:27 +0700

    config: constant :wrench:

---
+++ lib/config/constant.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+const kFontSizeXXL = 36.0;
+const kFontSizeXL = 22.0;
+const kFontSizeL = 20.0;
+const kFontSizeM = 18.0;
+const kFontSizeS = 16.0;
+const kFontSizeXS = 13.0;
+
+const kLineHeightL = 40.0;
+const kLineHeightM = 35.0;
+const kLineHeightS = 20.0;
+
+const kFontWeightBold = FontWeight.bold;
+const kFontWeightSemiBold = FontWeight.w600;
+const kFontWeightMedium = FontWeight.w500;
+const kFontWeightRegular = FontWeight.normal;
+const kFontWeightLight = FontWeight.w300;
+
+const kSizeXXXS = 2.0;
+const kSizeXXS = 4.0;
+const kSizeXS = 8.0;
+const kSizeS = 16.0;
+const kSizeM = 32.0;
+const kSizeL = 64.0;
+const kSizeXL = 128.0;
+const kSizeXXL = 216.0;
+
+const kBorderRadiusXXS = BorderRadius.all(Radius.circular(kSizeXXS));
+const kBorderRadiusXS = BorderRadius.all(Radius.circular(kSizeXS));
+const kBorderRadiusS = BorderRadius.all(Radius.circular(kSizeS));
+const kBorderRadiusM = BorderRadius.all(Radius.circular(kSizeM));
+const kBorderRadiusL = BorderRadius.all(Radius.circular(kSizeL));
+const kBorderRadiusXL = BorderRadius.all(Radius.circular(kSizeXL));
+const kBorderRadiusXXL = BorderRadius.all(Radius.circular(kSizeXXL));
+
+const kSizedBoxVerticalXXS = SizedBox(height: kSizeXXS);
+const kSizedBoxVerticalXS = SizedBox(height: kSizeXS);
+const kSizedBoxVerticalS = SizedBox(height: kSizeS);
+const kSizedBoxVerticalM = SizedBox(height: kSizeM);
+const kSizedBoxVerticalL = SizedBox(height: kSizeL);
+const kSizedBoxVerticalXL = SizedBox(height: kSizeXL);
+const kSizedBoxVerticalXXL = SizedBox(height: kSizeXXL);
+
+const kSizedBoxHorizontalXXS = SizedBox(width: kSizeXXS);
+const kSizedBoxHorizontalXS = SizedBox(width: kSizeXS);
+const kSizedBoxHorizontalS = SizedBox(width: kSizeS);
+const kSizedBoxHorizontalM = SizedBox(width: kSizeM);
+const kSizedBoxHorizontalL = SizedBox(width: kSizeL);
+const kSizedBoxHorizontalXL = SizedBox(width: kSizeXL);
+const kSizedBoxHorizontalXXL = SizedBox(width: kSizeXXL);
+
+const kSizedBoxXXS = SizedBox(height: kSizeXXS, width: kSizeXXS);
+const kSizedBoxXS = SizedBox(height: kSizeXS, width: kSizeXS);
+const kSizedBoxS = SizedBox(height: kSizeS, width: kSizeS);
+const kSizedBoxM = SizedBox(height: kSizeM, width: kSizeM);
+const kSizedBoxL = SizedBox(height: kSizeL, width: kSizeL);
+const kSizedBoxXL = SizedBox(height: kSizeXL, width: kSizeXL);
+const kSizedBoxXXL = SizedBox(height: kSizeXXL, width: kSizeXXL);
