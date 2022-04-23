10574f4a306253ba945bdb815d8940a2b7250b3f
coolkidneversleep <iiippbhy@gmail.com>
2022-04-23 11:37:21 +0700

    config: routes :wrench:

---
+++ lib/routes.dart
@@ -0,0 +1,33 @@
+class RoutesName{
+  // Onboarding
+  static const onboard = '/onboard';
+  static const login = '/onboard/login';
+  static const register = '/onboard/register';
+  static const registerProfile = '/onboard/register/profile';
+  static const registerTime = '/onboard/register/time';
+
+  // Home
+  static const home = '/home';
+  static const track = '/track';
+
+  // Customize track
+  static const customize = '/customize';
+  static const customizeSection = '/customize/section';
+  static const customizeExerciseSection = '/customize/section/exercise';
+  static const customizeBreakSection = '/customize/section/break';
+
+  // User
+  static const upload = '/upload';
+  static const library = '/library';
+  static const favorite = '/favorite';
+
+  // Setting
+  static const setting = '/setting';
+  static const settingProfile = '/setting/profile';
+  static const settingHelp = '/setting/help';
+  static const settingPreference = '/setting/preference';
+
+  // Forget Password
+  static const forgetPassword = '/forget-password';
+  static const newPassword = '/forget-password/new-password';
+}
