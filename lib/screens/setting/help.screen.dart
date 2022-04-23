9687bb5c00e9496351d632d5bfb6d6f9b590c850
coolkidneversleep <iiippbhy@gmail.com>
2022-04-23 12:27:13 +0700

    add: setting screens :sparkles:

---
+++ lib/screens/setting/help.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SettingHelpScreen extends StatelessWidget {
+  const SettingHelpScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Setting Help Screen'),
+    );
+  }
+}
---
+++ lib/screens/setting/preference.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SettingPreferenceScreen extends StatelessWidget {
+  const SettingPreferenceScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Setting Preference Screen'),
+    );
+  }
+}
