64104e609fbf4981c4a5d413f4bf7e30f060905b
coolkidneversleep <iiippbhy@gmail.com>
2022-04-23 11:49:34 +0700

    add: onboarding screens :sparkles:

---
+++ lib/screens/onboarding/register/time.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class RegisterTimeScreen extends StatelessWidget {
+  const RegisterTimeScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Register Time Screen'),
+    );
+  }
+}
