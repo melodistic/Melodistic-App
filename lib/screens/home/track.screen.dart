410451e2ab80e65ecd1adbb137d074ad96aabbd6
coolkidneversleep <iiippbhy@gmail.com>
2022-04-23 12:03:33 +0700

    add: home screen :sparkles:

---
+++ lib/screens/home/track.screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TrackScreen extends StatelessWidget {
+  const TrackScreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Track Screen'),
+    );
+  }
+}
