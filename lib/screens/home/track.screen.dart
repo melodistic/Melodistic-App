3c59cf43e638d673c2a0ed2f28367f9d9af616ee
GGolfz <wisarutgolf11@gmail.com>
2022-05-18 22:23:56 +0700

    add: track screen :sparkles:

--- lib/screens/home/track.screen.dart
+++ lib/screens/home/track.screen.dart
@@ -1,12 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:melodistic/widgets/common/screen-wrapper.widget.dart';
+import 'package:just_audio/just_audio.dart';
 
 class TrackScreen extends StatelessWidget {
-  const TrackScreen({ Key? key }) : super(key: key);
+  const TrackScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final arguments = (ModalRoute.of(context)?.settings.arguments ??
+        <String, dynamic>{}) as Map;
+    final player = AudioPlayer();
+    return ScreenWrapper(
+        child: Column(children: [
+      Text(arguments["filename"]),
+      MusicPlayer(player: player, filename: arguments["filename"])
+    ]));
+  }
+}
+
+class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({Key? key, required this.player, required this.filename})
+      : super(key: key);
+  final AudioPlayer player;
+  final String filename;
+  @override
+  State<MusicPlayer> createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  @override
+  void initState() {
+    try {
+      widget.player.setAudioSource(AudioSource.uri(
+          Uri.parse("http://20.24.147.227:5050/api/play/" + widget.filename)));
+    } catch (err) {
+      print(err);
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    widget.player.stop();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Track Screen'),
-    );
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      IconButton(
+          icon: Icon(Icons.play_arrow),
+          onPressed: () {
+            widget.player.play();
+          }),
+      IconButton(
+          icon: Icon(Icons.pause),
+          onPressed: () {
+            widget.player.pause();
+          }),
+    ]));
   }
 }
