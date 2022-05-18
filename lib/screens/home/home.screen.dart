761573ed5943318a5a24959485bd680803581f29
GGolfz <wisarutgolf11@gmail.com>
2022-05-18 22:23:48 +0700

    add: home screen :sparkles:

--- lib/screens/home/home.screen.dart
+++ lib/screens/home/home.screen.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:melodistic/routes.dart';
+import 'package:melodistic/widgets/common/screen-wrapper.widget.dart';
+import 'package:dio/dio.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({ Key? key }) : super(key: key);
+  const HomeScreen({Key? key}) : super(key: key);
+  Future<List> getPlaylist() async {
+    final Response response =
+        await Dio().get("http://20.24.147.227:5050/api/playlist");
+    final List data = response.data["files"];
+    return data;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home Screen'),
-    );
+    return ScreenWrapper(
+        isHome: true,
+        child: Container(
+          child: FutureBuilder(
+            future: getPlaylist(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                final data = snapshot.data as List;
+                return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      title: Text(data[index]),
+                      onTap: () {
+                        Navigator.of(context).pushNamed(RoutesName.track,
+                            arguments: {'filename': data[index]});
+                      },
+                    );
+                  },
+                );
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            },
+          ),
+        ));
   }
 }
