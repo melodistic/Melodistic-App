6501b04bb8d52d97d737a9d98b4781dbb5cbb2a2
GGolfz <wisarutgolf11@gmail.com>
2022-08-28 13:25:24 +0700

    [MEL-42] fix problem

--- lib/widgets/common/loading.widget.dart
+++ lib/widgets/common/loading.widget.dart
@@ -3,7 +3,6 @@ import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:melodistic/config/color.dart';
 import 'package:melodistic/config/constant.dart';
 import 'package:melodistic/config/style.dart';
-import 'package:melodistic/widgets/common/type/screen.type.dart';
 
 class LoadingWidget extends StatelessWidget {
   const LoadingWidget({
@@ -14,11 +13,10 @@ class LoadingWidget extends StatelessWidget {
     return Scaffold(
         body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Container(
-            child: const SpinKitFadingCircle(
+      children: <Widget>[
+        const SpinKitFadingCircle(
           color: kGrayScaleColor300,
-        )),
+        ),
         kSizedBoxVerticalS,
         Text('Loading', style: kBody1.copyWith(fontWeight: kFontWeightMedium))
       ],
