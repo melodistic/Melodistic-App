bffae4107bf8d907a04398593fc39fdfccc01099
GelidGrapesGG <Kittiyaporn.takrudthieng@mail.kmutt.ac.th>
2022-09-27 14:19:13 +0700

    [MEL-100] Updated Configuration Icons

--- lib/config/icon.dart
+++ lib/config/icon.dart
@@ -20,37 +20,37 @@ class MelodisticIcon {
       IconData(0xe804, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData cross =
       IconData(0xe805, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_look_top =
+  static const IconData edit_filled =
       IconData(0xe806, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_really =
+  static const IconData emoji_look_top =
       IconData(0xe807, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_sad =
+  static const IconData emoji_really =
       IconData(0xe808, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_satisfied =
+  static const IconData emoji_sad =
       IconData(0xe809, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_sing_right_note =
+  static const IconData emoji_satisfied =
       IconData(0xe80a, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData emoji_talking_happy =
+  static const IconData emoji_sing_right_note =
       IconData(0xe80b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData exit_left =
+  static const IconData emoji_talking_happy =
       IconData(0xe80c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData eye_empty =
+  static const IconData exit_left =
       IconData(0xe80d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData eye_off =
+  static const IconData eye_empty =
       IconData(0xe80e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData filter =
+  static const IconData eye_off =
       IconData(0xe80f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData folder_add =
+  static const IconData favorite_filled =
       IconData(0xe810, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData forward_15_seconds =
+  static const IconData favorite =
       IconData(0xe811, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData google =
+  static const IconData filter =
       IconData(0xe812, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData heart =
+  static const IconData folder_add =
       IconData(0xe813, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData home_door =
+  static const IconData forward_15_seconds =
       IconData(0xe814, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData iconoir_emoji_sing_right_note =
+  static const IconData home_door =
       IconData(0xe815, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData list_add =
       IconData(0xe816, fontFamily: _kFontFam, fontPackage: _kFontPkg);
@@ -66,14 +66,12 @@ class MelodisticIcon {
       IconData(0xe81b, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData pull_up =
       IconData(0xe81c, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData settings =
+  static const IconData running =
       IconData(0xe81d, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData system_uicons_arrow_right =
+  static const IconData settings =
       IconData(0xe81e, fontFamily: _kFontFam, fontPackage: _kFontPkg);
-  static const IconData system_uicons_check =
-      IconData(0xe81f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData warning_circle =
-      IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe81f, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData write =
-      IconData(0xe821, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+      IconData(0xe820, fontFamily: _kFontFam, fontPackage: _kFontPkg);
 }
