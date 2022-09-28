e3730529eb7dd9ed6bfe4e9c168a4f6410eb734c
GGolfz <wisarutgolf11@gmail.com>
2022-09-28 22:51:42 +0700

    [MEL-81] fix test

--- test/widgets/common/screen-wrapper.widget_test.dart
+++ test/widgets/common/screen-wrapper.widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:melodistic/singleton/controller.dart';
+import 'package:melodistic/widgets/common/appbar/main.widget.dart';
 import 'package:melodistic/widgets/common/screen-wrapper.widget.dart';
 
 import '../../utils.dart';
@@ -8,15 +9,11 @@ import '../../utils.dart';
 void main() {
   Controller.setup();
   testWidgets('Screen wrapper render correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(
-        createWidgetForTesting(child: const ScreenWrapper(child: SizedBox())));
+    await tester.pumpWidget(createWidgetForTesting(
+        child: const ScreenWrapper(
+      child: SizedBox(),
+      customAppbar: MainAppbar(title: 'Melodistic'),
+    )));
     expect(find.byType(AppBar), findsOneWidget);
-    expect(
-        getWidget(
-            tester,
-            getTypeDescentantByKey(const Key('melodistic-appbar'), Padding)
-                .at(1)),
-        isA<Padding>().having((Padding padding) => padding.child == null,
-            'Padding child should be null', true));
   });
 }
