354d82944b8527a83854e8bc8d46cc33cedfbf2d
coolkidneversleep <iiippbhy@gmail.com>
2022-09-29 21:05:37 +0700

    [MEL-102] edit format

--- lib/screens/forget-password/widgets/forget-popup.widget.dart
+++ lib/screens/forget-password/widgets/forget-popup.widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/route_manager.dart';
 import 'package:melodistic/config/color.dart';
 import 'package:melodistic/config/constant.dart';
 import 'package:melodistic/config/icon.dart';
