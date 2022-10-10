8ccb81802eb1cb260d96f47c7820478b51699bf5
coolkidneversleep <iiippbhy@gmail.com>
2022-10-10 11:08:26 +0700

    [MEL-82] delete unused import

--- test/widgets/common/trackbox.widget_test.dart
+++ test/widgets/common/trackbox.widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:melodistic/models/track.model.dart';
 import 'package:melodistic/screens/home/widgets/trackbox.widget.dart';
