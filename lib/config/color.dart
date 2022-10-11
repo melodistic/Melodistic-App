7686e99e9db964abafdd9bfdfe9d18af92053fc8
GGolfz <wisarutgolf11@gmail.com>
2022-10-12 00:57:38 +0700

    [MEL-116] color configuration :sparkles:

--- lib/config/color.dart
+++ lib/config/color.dart
@@ -1,18 +1,8 @@
 import 'package:flutter/material.dart';
 
 const Color kPrimaryColor = Color(0xFF101010);
-const Color kPrimaryColor900 = Color(0xFF282828);
-const Color kPrimaryColor700 = Color(0xFF404040);
-const Color kPrimaryColor500 = Color(0xFF585858);
-const Color kPrimaryColor300 = Color(0xFF707070);
-const Color kPrimaryColor100 = Color(0xFFCFCFCF);
 
 const Color kSecondaryColor = Color(0xFFFA8B44);
-const Color kSecondaryColor900 = Color(0xFFFB9757);
-const Color kSecondaryColor700 = Color(0xFFFCAE7C);
-const Color kSecondaryColor500 = Color(0xFFFCC5A1);
-const Color kSecondaryColor300 = Color(0x66FA8B44);
-const Color kSecondaryColor100 = Color(0x33FA8B44);
 
 const Color kGrayScaleColorBlack = Color(0xFF000000);
 const Color kGrayScaleColor800 = Color(0xFF424242);
@@ -26,15 +16,16 @@ const Color kGrayScaleColor100 = Color(0xFFF5F5F5);
 const Color kGrayScaleColorWhite = Color(0xFFFFFFFF);
 const Color kTransparent = Color(0x00000000);
 
-const Color kSuccessColor = Color(0xFF87E29B);
-const Color kWarningColor = Color(0xFFFCCF6F);
+const Color kSuccessColor = Color(0xFF55BD6C);
+const Color kWarningColor = Color(0xFFF9B54F);
 const Color kErrorColor = Color(0xFFFA5A5A);
 
+const Color kGradientBlackStart = Color(0xFF585858);
 const LinearGradient kGradientBlack = LinearGradient(
   begin: Alignment.topLeft,
   end: Alignment.bottomRight,
   colors: <Color>[
-    kPrimaryColor500,
+    kGradientBlackStart,
     kGrayScaleColorBlack,
   ],
 );
