7fb9296b6bcd62bd9fe54067add85d6f4b7144b3
GGolfz <wisarutgolf11@gmail.com>
2022-10-20 15:27:27 +0700

    [MEL-157] update home tab :sparkles:

--- lib/controller/hometab.controller.dart
+++ lib/controller/hometab.controller.dart
@@ -5,10 +5,10 @@ import 'package:melodistic/screens/home/type/hometab.type.dart';
 class HomeTabController extends GetxController {
   final List<HomeTab> tabs = <HomeTab>[
     HomeTab(id: 1, label: 'All', type: HomeTabType.all, position: 0),
-    HomeTab(id: 2, label: 'Running', type: HomeTabType.running, position: 0),
-    HomeTab(id: 3, label: 'Cardio', type: HomeTabType.cardio, position: 0),
-    HomeTab(id: 4, label: 'Yoga', type: HomeTabType.yoga, position: 0),
-    HomeTab(id: 5, label: 'Pilatis', type: HomeTabType.pilatis, position: 0)
+    HomeTab(id: 2, label: 'Aerobic', type: HomeTabType.running, position: 0),
+    HomeTab(id: 3, label: 'Strength', type: HomeTabType.cardio, position: 0),
+    HomeTab(id: 4, label: 'Flexibility', type: HomeTabType.yoga, position: 0),
+    HomeTab(id: 5, label: 'Balancing', type: HomeTabType.pilatis, position: 35)
   ];
   Rx<HomeTab>? selectedTab;
   HomeTabController() {
