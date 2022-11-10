3e6153217c526a1d6eeda30e4f1b6a5c5ca945de
coolkidneversleep <iiippbhy@gmail.com>
2022-11-10 16:42:56 +0700

    [MEL-190] fix overflow

--- lib/screens/onboarding/login.screen.dart
+++ lib/screens/onboarding/login.screen.dart
@@ -22,131 +22,140 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenWrapper(
-        extendBodyBehindAppBar: true,
-        child: Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              image: DecorationImage(
-                alignment: Alignment.topCenter,
-                fit: BoxFit.contain,
-                image: AssetImage('assets/images/login.png'),
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: ScreenWrapper(
+          extendBodyBehindAppBar: true,
+          child: Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  alignment: Alignment.topCenter,
+                  fit: BoxFit.contain,
+                  image: AssetImage('assets/images/login.png'),
+                ),
               ),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Container(
-                    height: 455,
-                    width: double.infinity,
-                    decoration: const BoxDecoration(
-                      color: kGrayScaleColorWhite,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(kSizeS),
-                          topRight: Radius.circular(kSizeS)),
-                    ),
-                    child: Padding(
-                        padding: const EdgeInsets.fromLTRB(
-                            kSizeM, kSizeXS, kSizeM, kSizeS),
-                        child: Form(
-                            key: _authController.loginFormKey,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: <Widget>[
-                                Expanded(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.end,
-                                    children: <Widget>[
-                                      TextFieldWidget(
-                                        controller: _emailController,
-                                        hintTitle: 'Email',
-                                        fieldType: FieldType.text,
-                                        validate: _authController.validateEmail,
-                                      ),
-                                      TextFieldWidget(
-                                        controller: _passwordController,
-                                        hintTitle: 'Password',
-                                        fieldType: FieldType.password,
-                                        validate:
-                                            _authController.validatePassword,
-                                      ),
-                                      Padding(
-                                        padding:
-                                            const EdgeInsets.only(top: kSizeXS),
-                                        child: ButtonWidget(
-                                          size: ButtonSize.small,
-                                          button: ButtonType.textButton,
-                                          customContent: Text(
-                                              'Forget the Password?',
-                                              style: kBody3.copyWith(
-                                                  color: kGrayScaleColor600)),
-                                          handleClick: () {
-                                            Get.toNamed<dynamic>(
-                                                RoutesName.forgetPassword);
-                                          },
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                ),
-                                Padding(
-                                    padding:
-                                        const EdgeInsets.only(bottom: kSizeS),
-                                    child: ButtonWidget(
-                                        size: ButtonSize.large,
-                                        button: ButtonType.mainButton,
-                                        state: ButtonState.normal,
-                                        handleClick: () async {
-                                          if (_authController
-                                              .loginFormKey.currentState!
-                                              .validate()) {
-                                            String email =
-                                                _emailController.text;
-                                            String password =
-                                                _passwordController.text;
-                                            bool result = await _authController
-                                                .login(email, password);
-                                            if (result) {
-                                              Get.toNamed<dynamic>(
-                                                  RoutesName.home);
-                                            }
-                                          }
-                                        },
-                                        text: 'Log in')),
-                                ButtonWidget(
-                                    size: ButtonSize.large,
-                                    button: ButtonType.softButton,
-                                    state: ButtonState.normal,
-                                    handleClick: _authController.authWithGoogle,
-                                    prefixIcon: MelodisticIcon.google,
-                                    text: ' Log in with Google'),
-                                kSizedBoxVerticalXS,
-                                SizedBox(
-                                    width: double.infinity,
-                                    height: 48,
-                                    child: Row(
-                                        mainAxisAlignment:
-                                            MainAxisAlignment.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  SingleChildScrollView(
+                    keyboardDismissBehavior:
+                        ScrollViewKeyboardDismissBehavior.onDrag,
+                    physics: const ClampingScrollPhysics(),
+                    child: Container(
+                        height: 400,
+                        width: double.infinity,
+                        decoration: const BoxDecoration(
+                          color: kGrayScaleColorWhite,
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(kSizeS),
+                              topRight: Radius.circular(kSizeS)),
+                        ),
+                        child: Padding(
+                            padding: const EdgeInsets.fromLTRB(
+                                kSizeM, kSizeXXS, kSizeM, kSizeXXS),
+                            child: Form(
+                                key: _authController.loginFormKey,
+                                child: Column(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: <Widget>[
+                                    Flexible(
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.end,
                                         children: <Widget>[
-                                          Text('Don\'t have any account?',
-                                              style: kBody3.copyWith(
-                                                  color: kGrayScaleColor500)),
-                                          kSizedBoxHorizontalXXS,
-                                          GestureDetector(
-                                              onTap: () {
-                                                Get.toNamed<dynamic>(
-                                                    RoutesName.register);
-                                              },
-                                              child: Text('Register',
-                                                  style:
-                                                      kBody3SemiBold.copyWith(
-                                                          color:
-                                                              kSecondaryColor)))
-                                        ])),
-                              ],
-                            )))),
-              ],
-            )));
+                                          TextFieldWidget(
+                                            controller: _emailController,
+                                            hintTitle: 'Email',
+                                            fieldType: FieldType.text,
+                                            validate:
+                                                _authController.validateEmail,
+                                          ),
+                                          TextFieldWidget(
+                                            controller: _passwordController,
+                                            hintTitle: 'Password',
+                                            fieldType: FieldType.password,
+                                            validate: _authController
+                                                .validatePassword,
+                                          ),
+                                          ButtonWidget(
+                                            size: ButtonSize.small,
+                                            button: ButtonType.textButton,
+                                            customContent: Text(
+                                                'Forget the Password?',
+                                                style: kBody3.copyWith(
+                                                    color: kGrayScaleColor600)),
+                                            handleClick: () {
+                                              Get.toNamed<dynamic>(
+                                                  RoutesName.forgetPassword);
+                                            },
+                                          ),
+                                        ],
+                                      ),
+                                    ),
+                                    Padding(
+                                        padding: const EdgeInsets.only(
+                                            bottom: kSizeXXS),
+                                        child: ButtonWidget(
+                                            size: ButtonSize.large,
+                                            button: ButtonType.mainButton,
+                                            state: ButtonState.normal,
+                                            handleClick: () async {
+                                              if (_authController
+                                                  .loginFormKey.currentState!
+                                                  .validate()) {
+                                                String email =
+                                                    _emailController.text;
+                                                String password =
+                                                    _passwordController.text;
+                                                bool result =
+                                                    await _authController.login(
+                                                        email, password);
+                                                if (result) {
+                                                  Get.toNamed<dynamic>(
+                                                      RoutesName.home);
+                                                }
+                                              }
+                                            },
+                                            text: 'Log in')),
+                                    ButtonWidget(
+                                        size: ButtonSize.large,
+                                        button: ButtonType.softButton,
+                                        state: ButtonState.normal,
+                                        handleClick:
+                                            _authController.authWithGoogle,
+                                        prefixIcon: MelodisticIcon.google,
+                                        text: ' Log in with Google'),
+                                    kSizedBoxVerticalXS,
+                                    SizedBox(
+                                        width: double.infinity,
+                                        height: 30,
+                                        child: Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            children: <Widget>[
+                                              Text('Don\'t have any account?',
+                                                  style: kBody3.copyWith(
+                                                      color:
+                                                          kGrayScaleColor500)),
+                                              kSizedBoxHorizontalXXS,
+                                              GestureDetector(
+                                                  onTap: () {
+                                                    Get.toNamed<dynamic>(
+                                                        RoutesName.register);
+                                                  },
+                                                  child: Text('Register',
+                                                      style: kBody3SemiBold
+                                                          .copyWith(
+                                                              color:
+                                                                  kSecondaryColor)))
+                                            ])),
+                                  ],
+                                )))),
+                  ),
+                ],
+              ))),
+    );
   }
 }
