8769b4e7bcc27d74da6981e93125367e661d3d26
coolkidneversleep <iiippbhy@gmail.com>
2022-11-11 12:18:06 +0700

    [MEL-198] overflow in validate OTP screen

--- lib/screens/forget-password/validate-otp.screen.dart
+++ lib/screens/forget-password/validate-otp.screen.dart
@@ -20,49 +20,66 @@ class ValidateOtpScreen extends StatelessWidget {
       customAppbar: const BackAppbar(
         title: 'Validate OTP',
       ),
-      child: Padding(
-        padding:
-            const EdgeInsets.fromLTRB(kSizeM, kSizeS, kSizeS * 1.5, kSizeXXXS),
-        child: Form(
-            key: forgetPasswordController.otpValidateFormKey,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Image.asset('assets/images/forget_pwd.png'),
-                  kSizedBoxVerticalL,
-                  Padding(
-                      padding: const EdgeInsets.only(right: kSizeM),
-                      child: Obx(() => Text(
-                            'A One Time Passcode has been sent to ${forgetPasswordController.email.value}',
-                            style: kBody2,
-                          ))),
-                  kSizedBoxVerticalS,
-                  const Text(
-                    'Please check your e-mail and enter the OTP below to verify your email address. If you cannot see the email, make sure to check your SPAM folder.',
-                    style: kBody2,
-                  ),
-                  TextFieldWidget(
-                      hintTitle: 'OTP',
-                      controller: otpController,
-                      validate: forgetPasswordController.validateOTP),
-                  kSizedBoxVerticalL,
-                  ButtonWidget(
-                    button: ButtonType.mainButton,
-                    text: 'Verify',
-                    handleClick: () async {
-                      if (forgetPasswordController
-                          .otpValidateFormKey.currentState!
-                          .validate()) {
-                        bool success = await forgetPasswordController
-                            .verifyOTP(otpController.text);
-                        if (success) {
-                          Get.toNamed<void>(RoutesName.newPassword);
-                        }
-                      }
-                    },
-                  )
-                ])),
-      ),
+      child: CustomScrollView(slivers: <Widget>[
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(
+                kSizeM, kSizeS, kSizeS * 1.5, kSizeXXS),
+            child: Form(
+                key: forgetPasswordController.otpValidateFormKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset(
+                              'assets/images/forget_pwd.png',
+                              height: 200,
+                            ),
+                            kSizedBoxVerticalS,
+                            Padding(
+                                padding: const EdgeInsets.only(right: kSizeM),
+                                child: Obx(() => Text(
+                                      'A One Time Passcode has been sent to ${forgetPasswordController.email.value}',
+                                      style: kBody2,
+                                    ))),
+                            kSizedBoxVerticalS,
+                            const Text(
+                              'Please check your e-mail and enter the OTP below to verify your email address. If you cannot see the email, make sure to check your SPAM folder.',
+                              style: kBody2,
+                            ),
+                            TextFieldWidget(
+                                hintTitle: 'OTP',
+                                controller: otpController,
+                                validate: forgetPasswordController.validateOTP),
+                          ],
+                        ),
+                      ),
+                      kSizedBoxVerticalM,
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 20),
+                        child: ButtonWidget(
+                          button: ButtonType.mainButton,
+                          text: 'Verify',
+                          handleClick: () async {
+                            if (forgetPasswordController
+                                .otpValidateFormKey.currentState!
+                                .validate()) {
+                              bool success = await forgetPasswordController
+                                  .verifyOTP(otpController.text);
+                              if (success) {
+                                Get.toNamed<void>(RoutesName.newPassword);
+                              }
+                            }
+                          },
+                        ),
+                      )
+                    ])),
+          ),
+        )
+      ]),
     );
   }
 }
