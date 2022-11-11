1c3767851d3edbc2eb7a77b93226d9285cd89b19
coolkidneversleep <iiippbhy@gmail.com>
2022-11-11 07:49:46 +0700

    [MEl-196] overflow in forget password

--- lib/screens/forget-password/forget-password.screen.dart
+++ lib/screens/forget-password/forget-password.screen.dart
@@ -21,51 +21,58 @@ class ForgetPasswordScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenWrapper(
       customAppbar: const BackAppbar(title: 'Forget Password'),
-      child: Padding(
-        padding: const EdgeInsets.fromLTRB(kSizeM, kSizeS, kSizeM, kSizeXXXS),
-        child: Form(
-            key: forgetPasswordController.requestResetPasswordFormKey,
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    child: Column(
-                      children: <Widget>[
-                        Image.asset('assets/images/forget_pwd.png'),
-                        kSizedBoxVerticalL,
-                        const Padding(
-                            padding: EdgeInsets.only(right: kSizeM),
-                            child: Text(
-                              "Provide your account's email for which you want to reset your password",
-                              style: kBody2,
-                            )),
-                        TextFieldWidget(
-                            hintTitle: 'Email',
-                            controller: emailController,
-                            fieldType: FieldType.text,
-                            validate: forgetPasswordController.validateEmail)
-                      ],
-                    ),
-                  ),
-                  ButtonWidget(
-                      button: ButtonType.mainButton,
-                      text: 'Send reset password link',
-                      handleClick: () async {
-                        String email = emailController.text;
-                        if (forgetPasswordController
-                            .requestResetPasswordFormKey.currentState!
-                            .validate()) {
-                          bool success = await forgetPasswordController
-                              .configRequestResetPassword(email);
-                          if (success) {
-                            Alert.showAlert(ForgetPopup(), action: () {
-                              Get.toNamed<void>(RoutesName.validateOtp);
-                            });
-                          }
-                        }
-                      })
-                ])),
-      ),
+      child: CustomScrollView(slivers: <Widget>[
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding:
+                const EdgeInsets.fromLTRB(kSizeM, kSizeS, kSizeM, kSizeXXXS),
+            child: Form(
+                key: forgetPasswordController.requestResetPasswordFormKey,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: Column(
+                          children: <Widget>[
+                            Image.asset('assets/images/forget_pwd.png'),
+                            kSizedBoxVerticalL,
+                            const Padding(
+                                padding: EdgeInsets.only(right: kSizeM),
+                                child: Text(
+                                  "Provide your account's email for which you want to reset your password",
+                                  style: kBody2,
+                                )),
+                            TextFieldWidget(
+                                hintTitle: 'Email',
+                                controller: emailController,
+                                fieldType: FieldType.text,
+                                validate:
+                                    forgetPasswordController.validateEmail)
+                          ],
+                        ),
+                      ),
+                      ButtonWidget(
+                          button: ButtonType.mainButton,
+                          text: 'Send reset password link',
+                          handleClick: () async {
+                            String email = emailController.text;
+                            if (forgetPasswordController
+                                .requestResetPasswordFormKey.currentState!
+                                .validate()) {
+                              bool success = await forgetPasswordController
+                                  .configRequestResetPassword(email);
+                              if (success) {
+                                Alert.showAlert(ForgetPopup(), action: () {
+                                  Get.toNamed<void>(RoutesName.validateOtp);
+                                });
+                              }
+                            }
+                          })
+                    ])),
+          ),
+        )
+      ]),
     );
   }
 }
